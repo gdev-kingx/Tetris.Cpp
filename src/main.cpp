54c0495d4ab9c1cefb76b365edd0257d87d837c2
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2024-04-19 12:51:35 +0530

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include <raylib.h>
+#include "game.h"
+#include "colors.h"
+#include <iostream>
+
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    InitWindow(500, 620, "Tetris");
+    SetTargetFPS(60);
+
+    Font font = LoadFontEx("Font/monogram.ttf", 64, 0, 0);
+
+    Game game = Game();
+
+    while (WindowShouldClose() == false)
+    {
+        UpdateMusicStream(game.music);
+        game.HandleInput();
+        if (EventTriggered(0.2))
+        {
+            game.MoveBlockDown();
+        }
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawTextEx(font, "Score", {365, 15}, 38, 2, WHITE);
+        DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
+        if (game.gameOver)
+        {
+            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
+        }
+        DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, WHITE);
+
+        char scoreText[10];
+        sprintf(scoreText, "%d", game.score);
+        Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
+
+        DrawTextEx(font, scoreText, {320 + (170 - textSize.x) / 2, 65}, 38, 2, BLACK);
+        DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, WHITE);
+        game.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
